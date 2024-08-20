0eaa07b617a0442071cbd2f1e34e8fabaaa4416f
gildembergleite <gildembergleite@gmail.com>
2024-08-20 20:04:17 -0300

    refactor: export build files

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export { BaseApi, BaseApiConstructorProps } from './dist/api/base-api'
+export { FetchHttpClient } from './dist/api/fetch-http-client'
+export { HttpClient } from './dist/interfaces/http-client'
+export { RequestHeaders } from './dist/interfaces/request-headers'
+export { RequestParams } from './dist/interfaces/request-params'
+export { RequestOptions } from './dist/interfaces/requests-options'
+export { UrlBuilder } from './dist/utils/url-builder'
+
--- index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-export { BaseApi, BaseApiConstructorProps } from './src/api/base-api'
-export { FetchHttpClient } from './src/api/fetch-http-client'
-export { HttpClient } from './src/interfaces/http-client'
-export { RequestHeaders } from './src/interfaces/request-headers'
-export { RequestParams } from './src/interfaces/request-params'
-export { RequestOptions } from './src/interfaces/requests-options'
-export { UrlBuilder } from './src/utils/url-builder'
-
