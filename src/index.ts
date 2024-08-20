b05565fecc2f3ad3af858799efccd1161cf9875b
gildembergleite <gildembergleite@gmail.com>
2024-08-20 19:39:47 -0300

    refactor: move index file

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-export { BaseApi, BaseApiConstructorProps } from './api/base-api'
-export { FetchHttpClient } from './api/fetch-http-client'
-export { HttpClient } from './interfaces/http-client'
-export { RequestHeaders } from './interfaces/request-headers'
-export { RequestParams } from './interfaces/request-params'
-export { RequestOptions } from './interfaces/requests-options'
-export { UrlBuilder } from './utils/url-builder'
-
